ea97799a0aa9b4857de94a53968607dd7a37084e
SankhaDatta <sdatta.vedts@gmail.com>
2024-12-24 16:24:34 +0530

    commit by sankha

--- src/main/java/com/divine/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/divine/ecommerce/config/MyDataRestConfig.java
@@ -1,5 +1,10 @@
 package com.divine.ecommerce.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -9,26 +14,46 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import com.divine.ecommerce.model.Product;
 import com.divine.ecommerce.model.ProductCategory;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
+	private EntityManager entitymanger;
+	
+	@Autowired
+	public MyDataRestConfig (EntityManager entitymanger) {
+		this.entitymanger=entitymanger;
+	}
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 		// TODO Auto-generated method stub
 
-		HttpMethod[]theUnsupportedActions = {HttpMethod.PUT,HttpMethod.POST,HttpMethod.DELETE};
-		
-		
+		HttpMethod[]unSupportedMethods = {HttpMethod.POST,HttpMethod.PUT,HttpMethod.DELETE};
 		config.getExposureConfiguration()
-				.forDomainType(Product.class)
-				.withItemExposure((metadata,httpmethods)->httpmethods.disable(theUnsupportedActions))
-				.withCollectionExposure((metadata,httpmethods)->httpmethods.disable(theUnsupportedActions));
+		.forDomainType(Product.class)
+		.withItemExposure((metadata,httpmethod)->httpmethod.disable(unSupportedMethods))
+		.withCollectionExposure((metadata,httpmethod)->httpmethod.disable(unSupportedMethods));
 		
 		config.getExposureConfiguration()
 		.forDomainType(ProductCategory.class)
-		.withItemExposure((metadata,httpmethods)->httpmethods.disable(theUnsupportedActions))
-		.withCollectionExposure((metadata,httpmethods)->httpmethods.disable(theUnsupportedActions));
+		.withItemExposure((metadata,httpmethod)->httpmethod.disable(unSupportedMethods))
+		.withCollectionExposure((metadata,httpmethod)->httpmethod.disable(unSupportedMethods));
 		
+		exposIds(config);
+	}
+	private void exposIds(RepositoryRestConfiguration config) {
+		
+		// expose id
+		// get a list of all enity classes
+		Set<EntityType<?>>entities = entitymanger.getMetamodel().getEntities();
+		List<Class>entityClasses = new ArrayList<>();
+		for(EntityType ent:entities) {
+			entityClasses.add(ent.getJavaType());
+		}
+		Class[]domainTypes = entityClasses.toArray(new Class[0]);
+		config.exposeIdsFor(domainTypes);
 	}
 	
 }
--- src/main/java/com/divine/ecommerce/dao/ProductCategoryInterface.java
+++ src/main/java/com/divine/ecommerce/dao/ProductCategoryInterface.java
@@ -2,9 +2,11 @@ package com.divine.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.divine.ecommerce.model.ProductCategory;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productcategory" ,path = "product-category")
 public interface ProductCategoryInterface  extends JpaRepository<ProductCategory, Long>{
 }
